55c15f5f2508c85d593495562504a36b336e67aa
necrobsd <necrobsd@gmail.com>
2017-12-05 14:51:46 +1000

    celery

--- bot/tasks.py
+++ bot/tasks.py
@@ -14,55 +14,51 @@ TIMEOUT = 1/25
 @app.task(bind=True, default_retry_delay=300, max_retries=5)
 # @app.task()
 def send_message(text_message=None, photo_message=None):
-    try:
-        bot = DjangoTelegramBot.get_bot()
-        if photo_message:
-            try:
-                message = PhotoMessages.objects.get(id=photo_message)
-                image_url = settings.DJANGO_TELEGRAMBOT.get('WEBHOOK_SITE') + message.image.url
-                for counter, subscriber in enumerate(message.group.subscribers.all()):
-                    time.sleep(TIMEOUT)
-                    if not counter:
-                        try:
-                            response = bot.sendPhoto(
-                                subscriber.subscriber.chat_id,
-                                # 472186134,
-                                photo=image_url,
-                                caption=message.text)
-                            print(response)
-                            file_id = response.photo[-1].file_id
-                        except TelegramError as error:
-                            print(error.message)
-                    else:
-                        bot.sendPhoto(
-                            subscriber.subscriber.chat_id,
-                            # 472186134,
-                            photo=file_id if file_id else image_url,
-                            caption=message.text)
-                message.status = True
-                message.save()
-            except ObjectDoesNotExist:
-                pass
-
-        if text_message:
-            try:
-                message = TextMessages.objects.get(id=text_message)
-                text = '*{}*\n_{}_'.format(message.group.name, message.text)
-                for subscriber in message.group.subscribers.all():
-                    time.sleep(TIMEOUT)
+    bot = DjangoTelegramBot.get_bot()
+    if photo_message:
+        try:
+            message = PhotoMessages.objects.get(id=photo_message)
+            image_url = settings.DJANGO_TELEGRAMBOT.get('WEBHOOK_SITE') + message.image.url
+            for counter, subscriber in enumerate(message.group.subscribers.all()):
+                time.sleep(TIMEOUT)
+                if not counter:
                     try:
-                        response = bot.sendMessage(
+                        response = bot.sendPhoto(
                             subscriber.subscriber.chat_id,
                             # 472186134,
-                            text, parse_mode='Markdown')
+                            photo=image_url,
+                            caption=message.text)
                         print(response)
+                        file_id = response.photo[-1].file_id
                     except TelegramError as error:
                         print(error.message)
-                message.status = True
-                message.save()
-            except ObjectDoesNotExist:
-                pass
+                else:
+                    bot.sendPhoto(
+                        subscriber.subscriber.chat_id,
+                        # 472186134,
+                        photo=file_id if file_id else image_url,
+                        caption=message.text)
+            message.status = True
+            message.save()
+        except ObjectDoesNotExist:
+            pass
 
-    except Exception as e:
-        print("maybe do some clenup here....")
-        self.retry(e)
+    if text_message:
+        try:
+            message = TextMessages.objects.get(id=text_message)
+            text = '*{}*\n_{}_'.format(message.group.name, message.text)
+            for subscriber in message.group.subscribers.all():
+                time.sleep(TIMEOUT)
+                try:
+                    response = bot.sendMessage(
+                        subscriber.subscriber.chat_id,
+                        # 472186134,
+                        text, parse_mode='Markdown')
+                    print(response)
+                except TelegramError as error:
+                    print(error.message)
+            message.status = True
+            message.save()
+        except ObjectDoesNotExist:
+            pass
+
