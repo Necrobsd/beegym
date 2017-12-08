dcf039681e994d90c5d26af21539d564b0ce9403
necrobsd <necrobsd@gmail.com>
2017-12-08 10:30:47 +1000

    request.user

--- bot_stat/views.py
+++ bot_stat/views.py
@@ -86,4 +86,4 @@ def get_stats(request):
     )
 
     # Step 3: Send the chart object to the template.
-    return render_to_response(request, 'bot_stat/bot_stat.html', {'stats': cht, 'form': form})
+    return render(request, 'bot_stat/bot_stat.html', {'stats': cht, 'form': form})
