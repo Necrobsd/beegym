91a97d2176d1ebf3d8c6e00432d787e4acb64c6a
necrobsd <necrobsd@gmail.com>
2018-01-23 12:07:30 +1000

    Статистика использования команд бота css стили

--- bot_stat/templatetags/get_count_of_used_functions.py
+++ bot_stat/templatetags/get_count_of_used_functions.py
@@ -8,8 +8,6 @@ register = template.Library()
 @register.filter(name='get_count')
 def get_count(subscriber, function):
     try:
-        print(subscriber, 'function =', function)
-        print('result_count=', subscriber.get(function=function).count)
         return subscriber.get(function=function).count
     except:
         return 0
