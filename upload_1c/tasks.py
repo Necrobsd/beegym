dbfd360edd1a9131f60636022e49d0f611fc2d65
necrobsd <necrobsd@gmail.com>
2018-02-12 11:51:49 +1000

    проверка, что в названии абонемента содержится слово "абонемент", иначе (если это разовое занятие) не сохраняем в базе

--- upload_1c/tasks.py
+++ upload_1c/tasks.py
@@ -24,21 +24,22 @@ def load_to_db(filename):
             data = json.load(f)
             for card_number, values in data.items():
                 if values['name']:
+                    if values['name'].lower().find("абонемент") != -1:
                     # print(card_number, 'Дата истечения абонемента: ',
                     #       values['exp_date'], 'Название абонемента: ', values['name'])
-                    try:
-                        card = Cards.objects.get(card_number=card_number)
-                        if card.exp_date != values['exp_date']:
-                            card.exp_date = values['exp_date']
-                            card.is_active = values['is_active']
-                            card.name = values['name']
-                            card.save()
-                    except ObjectDoesNotExist:
-                        Cards.objects.create(
-                            card_number=card_number,
-                            exp_date=values['exp_date'],
-                            is_active=values['is_active'],
-                            name=values['name'])
+                        try:
+                            card = Cards.objects.get(card_number=card_number)
+                            if card.exp_date != values['exp_date']:
+                                card.exp_date = values['exp_date']
+                                card.is_active = values['is_active']
+                                card.name = values['name']
+                                card.save()
+                        except ObjectDoesNotExist:
+                            Cards.objects.create(
+                                card_number=card_number,
+                                exp_date=values['exp_date'],
+                                is_active=values['is_active'],
+                                name=values['name'])
         # print('Сохранение данных в базу завершено. Удаляем полученный файл ', file)
         # os.remove(file)
     else:
